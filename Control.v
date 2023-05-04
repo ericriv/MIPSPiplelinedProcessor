d45b657f3ab0c1e651d15e00b624889008980149
ericriv88 <ericriv88@gmail.com>
2023-05-04 16:16:27 -0700

    Update Control.v

--- Control.v
+++ Control.v
@@ -11,7 +11,7 @@ reg [1:0] ALUOp;
 
 always@(*) begin
   case(Instruction)
-    6'h0: begin
+    6'h0: begin //Rtype
       RegWrite = 1'b1;
       MemToReg = 1'b0;
       Branch = 1'b0;
@@ -21,6 +21,50 @@ always@(*) begin
       ALUOp = 2'b10;
       ALUSrc = 1'b0;
       IFFlush = 1'b0;
+    end
+    6'h23: begin //lw
+      RegWrite = 1'b1;
+      MemToReg = 1'b1;
+      Branch = 1'b0;
+      MemRead = 1'b1;
+      MemWrite = 1'b0;
+      RegDest = 1'b0;
+      ALUOp = 2'b00;
+      ALUSrc = 1'b1;
+      IFFlush = 1'b0;
+    end
+    6'h2b: begin //sw
+      RegWrite = 1'b0;
+      MemToReg = 1'b0;
+      Branch = 1'b0;
+      MemRead = 1'b0;
+      MemWrite = 1'b1;
+      RegDest = 1'b0;
+      ALUOp = 2'b00;
+      ALUSrc = 1'b1;
+      IFFlush = 1'b0;
+    end
+    6'h04: begin //beq
+      RegWrite = 1'b0;
+      MemToReg = 1'b0;
+      Branch = 1'b1;
+      MemRead = 1'b0;
+      MemWrite = 1'b0;
+      RegDest = 1'b0;
+      ALUOp = 2'b01;
+      ALUSrc = 1'b0;
+      IFFlush = 1'b0;
+    end
+    6'h02: begin //jump may be broken... implemented same as branch atm
+      RegWrite = 1'b0;
+      MemToReg = 1'b0;
+      Branch = 1'b1;
+      MemRead = 1'b0;
+      MemWrite = 1'b0;
+      RegDest = 1'b0;
+      ALUOp = 2'b01;
+      ALUSrc = 1'b0;
+      IFFlush = 1'b1;
     end
     default: begin
       {RegWrite, MemToReg, Branch, MemRead,
